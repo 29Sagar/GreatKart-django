364b71a2e5b8edcb4f816732674c03ba2bc2df92
29Sagar <sagargavde41@gmail.com>
2022-10-04 18:10:55 +0530

    add to cart functionality (partrially done)

---
+++ carts/admin.py
@@ -0,0 +1,8 @@
+from atexit import register
+from django.contrib import admin
+
+from carts.models import Cart, CartItem
+
+# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
